bf9e6204c6a0bc1188d781d56d023a9f80fcbaf7
Tafadzwa Ben <tbmukwepa@gmail.com>
2024-08-18 18:05:21 +0200

    created the city table seeder, added phone_code column to the country table

--- database/migrations/2024_08_17_175712_create_countries_table.php
+++ database/migrations/2024_08_17_175712_create_countries_table.php
@@ -14,6 +14,7 @@ return new class extends Migration
         Schema::create('countries', function (Blueprint $table) {
             $table->id();
             $table->string('code');
+            $table->string('phone_code');
             $table->string('name');
             $table->timestamps();
         });
