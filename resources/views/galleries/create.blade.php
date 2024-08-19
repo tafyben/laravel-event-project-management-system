ad835b57bf4a8f4998fd0de746157e7be393c52a
Tafadzwa Ben <tbmukwepa@gmail.com>
2024-08-19 19:14:22 +0200

    correcting gallery files

--- resources/views/galleries/create.blade.php
+++ resources/views/galleries/create.blade.php
@@ -2,7 +2,7 @@
     <x-slot name="header">
         <div class="flex justify-between">
             <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
-                {{ __('New Gallery') }}
+                {{ __('New Gallery.') }}
             </h2>
         </div>
     </x-slot>
--- resources/views/galleries/edit.blade.php
+++ resources/views/galleries/edit.blade.php
@@ -2,7 +2,7 @@
     <x-slot name="header">
         <div class="flex justify-between">
             <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
-                {{ __('Edit Gallery') }}
+                {{ __('Edit Main Gallery') }}
             </h2>
         </div>
     </x-slot>
