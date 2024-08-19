ce68af75982222cbfe02f4f47ad58231355256db
Tafadzwa Ben <tbmukwepa@gmail.com>
2024-08-19 19:40:55 +0200

    created and added values for the likesystem, savedevent,savedeventsystem, storecomment, and welcome controller blade files

---
+++ app/Http/Controllers/WelcomeController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Event;
+use Illuminate\Http\Request;
+
+class WelcomeController extends Controller
+{
+    public function __invoke()
+    {
+        $events = Event::with('country', 'tags')->where('start_date', '>=', today())->orderBy('created_at', 'desc')->get();
+
+        return view('welcome', compact('events'));
+    }
+}
