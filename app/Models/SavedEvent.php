84a96ceeaf47deef40c14f8d678795fbc7028889
Tafadzwa Ben <tbmukwepa@gmail.com>
2024-08-19 16:39:41 +0200

    added relationship to the Models

--- app/Models/SavedEvent.php
+++ app/Models/SavedEvent.php
@@ -4,8 +4,22 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class SavedEvent extends Model
 {
     use HasFactory;
+
+    protected $guarded = [];
+
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function event(): BelongsTo
+    {
+        return $this->belongsTo(Event::class);
+    }
+
 }
--- database/migrations/2024_08_17_180101_create_events_table.php
+++ database/migrations/2024_08_17_180101_create_events_table.php
@@ -24,7 +24,7 @@ return new class extends Migration
             $table->integer('num_tickets');
             $table->foreignId('user_id')->constrained()->cascadeOnDelete();
             $table->foreignId('country_id')->constrained();
-            $table->foreignId('city')->constrained();
+            $table->foreignId('city_id')->constrained();
             $table->timestamps();
         });
     }
