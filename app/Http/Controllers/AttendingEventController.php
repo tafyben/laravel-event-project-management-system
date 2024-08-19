aeb2f1f0468103c9afa8e8f7189e0c4775f01073
Tafadzwa Ben <tbmukwepa@gmail.com>
2024-08-19 19:26:28 +0200

    created attendingevent controller and modified the gallery and event controller files

---
+++ app/Http/Controllers/AttendingEventController.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class AttendingEventController extends Controller
+{
+    //
+}
--- app/Http/Controllers/EventsController.php
+++ app/Http/Controllers/EventsController.php
@@ -2,7 +2,14 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Country;
+use App\Models\Event;
+use App\Models\Tag;
+use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Storage;
+use Illuminate\Support\Str;
+use Illuminate\View\View;
 
 class EventsController extends Controller
 {
@@ -11,7 +18,8 @@ class EventsController extends Controller
      */
     public function index()
     {
-        return view('events.index');
+        $events = Event::with('country')->get();
+        return view('events.index', compact('events'));
     }
 
     /**
@@ -25,9 +33,21 @@ class EventsController extends Controller
     /**
      * Store a newly created resource in storage.
      */
-    public function store(Request $request)
+    public function store(CreateEventRequest $request): RedirectResponse
     {
-        //
+        if ($request->hasFile('image')) {
+
+            $data = $request->validated();
+            $data['image'] = Storage::putFile('events', $request->file('image'));
+            $data['user_id'] = auth()->id();
+            $data['slug'] = Str::slug($request->title);
+
+            $event = Event::create($data);
+            $event->tags()->attach($request->tags);
+            return to_route('events.index');
+        } else {
+            return back();
+        }
     }
 
     /**
@@ -41,24 +61,38 @@ class EventsController extends Controller
     /**
      * Show the form for editing the specified resource.
      */
-    public function edit(string $id)
+    public function edit(Event $event): View
     {
-        //
+        $countries = Country::all();
+        $tags = Tag::all();
+        return view('events.edit', compact('countries', 'tags', 'event'));
     }
 
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, string $id)
+    public function update(UpdateEventRequest $request, Event $event): RedirectResponse
     {
-        //
+        $data = $request->validated();
+        if ($request->hasFile('image')) {
+            Storage::delete($event->image);
+            $data['image'] = Storage::putFile('events', $request->file('image'));
+        }
+
+        $data['slug'] = Str::slug($request->title);
+        $event->update($data);
+        $event->tags()->sync($request->tags);
+        return to_route('events.index');
     }
 
     /**
      * Remove the specified resource from storage.
      */
-    public function destroy(string $id)
+    public function destroy(Event $event): RedirectResponse
     {
-        //
+        Storage::delete($event->image);
+        $event->tags()->detach();
+        $event->delete();
+        return to_route('events.index');
     }
 }
